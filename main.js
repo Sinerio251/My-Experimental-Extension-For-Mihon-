ef114f2c2ba4ae9187921ae05f88e4a21c380a71
Sinerio251 <aramalo781@gmail.com>
2025-04-25 18:35:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+// Função para buscar os dados do index.min.json
 async function getMangaDetails(mangaId) {
-     const response = await fetch('https://raw.githubusercontent.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/main/index.min.json');
-     const data = await response.json();
-     return data.mangas[0];
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/main/index.min.json');
+    const data = await response.json();
+    return data.mangas[0];
+  } catch (error) {
+    console.error('Erro ao carregar o mangá:', error);
+    return null;
+  }
 }
